ba08f92a58d4d22610727a0a37402b2d4d760b34
JD Martin <namtri@gmail.com>
2026-01-02 15:05:41 -0700

    moving the footer to a common file

--- js/site.js
+++ js/site.js
@@ -23,3 +23,12 @@
     });
   }
 })();
+
+fetch('/components/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading footer:', error);
+  });
